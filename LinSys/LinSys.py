695eec6143f8daa8414ff195f4dfd62cbae33719
Cameron <cedwards3054@gmail.com>
2015-08-09 13:53:56 -0500

    Namespace changes

--- LinSys/LinSys.py
+++ LinSys/LinSys.py
@@ -1,15 +1,30 @@
-import tkinter as tk
-from tkinter import ttk
+from tkinter import Tk
+from tkinter import Frame
+from tkinter import Menu
+from tkinter import Button
+from tkinter import Label
+from tkinter import Canvas
+from tkinter import Scale as Slider
+from tkinter import Listbox as List
+from tkinter import Entry as Input
+from tkinter import Text as Output
+from tkinter import StringVar as String
+from tkinter import IntVar as Int
+from tkinter import END
+from tkinter import RIDGE
+from tkinter import BROWSE
+from tkinter import HORIZONTAL
+from tkinter.ttk import Combobox as DropDown
 import AddProductionRuleDialog as dp
 import AddDrawingRuleDialog as dd
 import Rule
 import Draw
 import Generator
 
-class Application(tk.Frame):
+class Application(Frame):
 
     def __init__(self, master=None):
-        tk.Frame.__init__(self, master)
+        Frame.__init__(self, master)
         self.pack()
         self.createWidgets()
 
@@ -37,14 +52,14 @@ class Application(tk.Frame):
             return (l[0].strip(), tup)
 
     def RefreshLists(self):
-        self.list_prod.delete(0, tk.END)
-        self.list_draw.delete(0, tk.END)
+        self.list_prod.delete(0, END)
+        self.list_draw.delete(0, END)
         prod = self.format(Rule.getProductions())
         draw = self.format(Rule.getDrawings())
         for p in prod:
-            self.list_prod.insert(tk.END, p)
+            self.list_prod.insert(END, p)
         for d in draw:
-            self.list_draw.insert(tk.END, d)
+            self.list_draw.insert(END, d)
 
     def AddProductionRule(self, edit=None):
         rule = dp.AddProductionRuleDialog(self, edit).result
@@ -96,8 +111,8 @@ class Application(tk.Frame):
         self.output = Generator.convert(seed, n)
         self.generated = True
         self.text_output.config(state='normal')
-        self.text_output.delete(1.0, tk.END)
-        self.text_output.insert(tk.END, self.output)
+        self.text_output.delete(1.0, END)
+        self.text_output.insert(END, self.output)
         self.text_output.config(state='disabled')
 
     def draw(self, n, step=False):
@@ -195,46 +210,47 @@ class Application(tk.Frame):
         print(output)
 
     def load(self):
+        #TODO use list comprehensions to parse
         print("load");
 
     def makeMenuBar(self):
-        self.menubar = tk.Menu(self);
+        self.menubar = Menu(self);
         self.menubar.add_command(label="Save", command=self.save)
         self.menubar.add_command(label="Load", command=self.load)
         root.config(menu=self.menubar)
 
     def makeInputFrame(self):
-        self.inp_seed = tk.StringVar()
-        self.gen_value = tk.IntVar()
-        self.fram_input = tk.Frame(self, bd=2, relief=self.style, width=300, height=900)
-        self.fram_seed = tk.Frame(self.fram_input, bd=1, relief=self.style)
-        self.fram_prod = tk.Frame(self.fram_input, bd=1, relief=self.style)
-        self.fram_draw = tk.Frame(self.fram_input, bd=1, relief=self.style)
-        self.fram_slide = tk.Frame(self.fram_input, bd=1, relief=self.style)
-        self.fram_gen = tk.Frame(self.fram_input, bd=1, relief=self.style)
-        self.fram_output = tk.Frame(self.fram_input, bd=1, relief=self.style)
-        self.menu_gen = ttk.Combobox(self.fram_gen, textvariable= self.gen_value)
-        self.entr_seed = tk.Entry(self.fram_seed, textvariable= self.inp_seed)
-        self.text_output = tk.Text(self.fram_output, width=35, height=10)
-        self.list_prod = tk.Listbox(self.fram_prod, selectmode= tk.BROWSE, font="Courier 8", height=5)
-        self.list_draw = tk.Listbox(self.fram_draw, selectmode= tk.BROWSE, font="Courier 8", height=5)
-        self.slid_linesize = tk.Scale(self.fram_slide, from_=0.1, to=10.0, orient=tk.HORIZONTAL, resolution=0.1, length=180)
-        self.slid_timer = tk.Scale(self.fram_slide, from_=0, to=5, orient=tk.HORIZONTAL, resolution=0.05, length=180)
-        self.slid_angle = tk.Scale(self.fram_slide, from_=0, to=359, orient=tk.HORIZONTAL, length=180)
-        self.butt_prodAdd = tk.Button(self.fram_prod, text="Add", width=8, command= self.AddProductionRule)
-        self.butt_prodEdit = tk.Button(self.fram_prod, text="Edit", width=8, command= self.EditProductionRule)
-        self.butt_prodDelete = tk.Button(self.fram_prod, text="Delete", width=8, command= self.DeleteProductionRule)
-        self.butt_drawAdd = tk.Button(self.fram_draw, text="Add", width=8, command= self.AddDrawingRule)
-        self.butt_drawEdit = tk.Button(self.fram_draw, text="Edit", width=8, command= self.EditDrawingRule)
-        self.butt_drawDelete = tk.Button(self.fram_draw, text="Delete", width=8, command= self.DeleteDrawingRule)
-        tk.Label(self.fram_seed, text="Axiom:", width=8).grid(row=0, column=0)
-        tk.Label(self.fram_prod, text="Production\nRules:", width=8).grid(row=0, column=0)
-        tk.Label(self.fram_draw, text="Drawing\nRules:", width=8).grid(row=0, column=0)
-        tk.Label(self.fram_slide, text="Line Size:").grid(row=0, column=0)
-        tk.Label(self.fram_slide, text="Delay (ms):").grid(row=1, column=0)
-        tk.Label(self.fram_slide, text="Starting Angle:").grid(row=2, column=0)
-        tk.Label(self.fram_output, text="Output:").grid(row=0, column=0)
-        tk.Label(self.fram_gen, text="Generations:").grid(row=0, column=0)
+        self.inp_seed = String()
+        self.gen_value = Int()
+        self.fram_input = Frame(self, bd=2, relief=self.style, width=300, height=900)
+        self.fram_seed = Frame(self.fram_input, bd=1, relief=self.style)
+        self.fram_prod = Frame(self.fram_input, bd=1, relief=self.style)
+        self.fram_draw = Frame(self.fram_input, bd=1, relief=self.style)
+        self.fram_slide = Frame(self.fram_input, bd=1, relief=self.style)
+        self.fram_gen = Frame(self.fram_input, bd=1, relief=self.style)
+        self.fram_output = Frame(self.fram_input, bd=1, relief=self.style)
+        self.menu_gen = DropDown(self.fram_gen, textvariable= self.gen_value)
+        self.entr_seed = Input(self.fram_seed, textvariable= self.inp_seed)
+        self.text_output = Output(self.fram_output, width=35, height=10)
+        self.list_prod = List(self.fram_prod, selectmode= BROWSE, font="Courier 8", height=5)
+        self.list_draw = List(self.fram_draw, selectmode= BROWSE, font="Courier 8", height=5)
+        self.slid_linesize = Slider(self.fram_slide, from_=0.1, to=10.0, orient=HORIZONTAL, resolution=0.1, length=180)
+        self.slid_timer = Slider(self.fram_slide, from_=0, to=5, orient=HORIZONTAL, resolution=0.05, length=180)
+        self.slid_angle = Slider(self.fram_slide, from_=0, to=359, orient=HORIZONTAL, length=180)
+        self.butt_prodAdd = Button(self.fram_prod, text="Add", width=8, command= self.AddProductionRule)
+        self.butt_prodEdit = Button(self.fram_prod, text="Edit", width=8, command= self.EditProductionRule)
+        self.butt_prodDelete = Button(self.fram_prod, text="Delete", width=8, command= self.DeleteProductionRule)
+        self.butt_drawAdd = Button(self.fram_draw, text="Add", width=8, command= self.AddDrawingRule)
+        self.butt_drawEdit = Button(self.fram_draw, text="Edit", width=8, command= self.EditDrawingRule)
+        self.butt_drawDelete = Button(self.fram_draw, text="Delete", width=8, command= self.DeleteDrawingRule)
+        Label(self.fram_seed, text="Axiom:", width=8).grid(row=0, column=0)
+        Label(self.fram_prod, text="Production\nRules:", width=8).grid(row=0, column=0)
+        Label(self.fram_draw, text="Drawing\nRules:", width=8).grid(row=0, column=0)
+        Label(self.fram_slide, text="Line Size:").grid(row=0, column=0)
+        Label(self.fram_slide, text="Delay (ms):").grid(row=1, column=0)
+        Label(self.fram_slide, text="Starting Angle:").grid(row=2, column=0)
+        Label(self.fram_output, text="Output:").grid(row=0, column=0)
+        Label(self.fram_gen, text="Generations:").grid(row=0, column=0)
 
         self.gen_value.set(1)
         self.menu_gen['values'] = tuple(range(1, 13))
@@ -264,29 +280,29 @@ class Application(tk.Frame):
         self.text_output.grid(row=1, column=0)
 
     def makeCanvasFrame(self):
-        self.fram_canvas = tk.Frame(self, bd=10, relief=self.style)
-        self.canvas = tk.Canvas(self.fram_canvas, width=900, height=500)
+        self.fram_canvas = Frame(self, bd=10, relief=self.style)
+        self.canvas = Canvas(self.fram_canvas, width=900, height=500)
         self.fram_canvas.grid(row=0, column=1, sticky='nesw')
         self.canvas.grid(sticky='nesw')
         self.canvas.bind("<Button-1>", self.click)
 
     def makeIgnitionFrame(self):
-        self.fram_ignition = tk.Frame(self, bd=4, relief=self.style)
-        self.butt_generate = tk.Button(self.fram_ignition, text=" -- GENERATE -- ", width=100, command= self.generate)
-        self.butt_draw = tk.Button(self.fram_ignition, text=" -- DRAW -- ", width=100, command= self.drawAll)
+        self.fram_ignition = Frame(self, bd=4, relief=self.style)
+        self.butt_generate = Button(self.fram_ignition, text=" -- GENERATE -- ", width=100, command= self.generate)
+        self.butt_draw = Button(self.fram_ignition, text=" -- DRAW -- ", width=100, command= self.drawAll)
         self.fram_ignition.grid(row=1, column=0, columnspan=2)
         self.butt_generate.grid()
         self.butt_draw.grid()
 
     def createWidgets(self):
-        self.style = tk.RIDGE
+        self.style = RIDGE
         self.startingPoint = (20, 20)
         self.makeMenuBar()
         self.makeInputFrame()
         self.makeCanvasFrame()
         self.makeIgnitionFrame()
 
-root = tk.Tk()
+root = Tk()
 root.title("Lindenmayer Systems")
 root.geometry("1270x680+0+0")
 #root.wm_iconbitmap('icon.ico')
